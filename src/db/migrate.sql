1eda61b8666d9a15278e69eca8dcc8a2eac7ec9c
Martin Borg <martin.d@live.se>
2019-09-11 18:04:30 +0200

    added trigger on price updates

--- src/db/migrate.sql
+++ src/db/migrate.sql
@@ -3,18 +3,40 @@ CREATE TABLE IF NOT EXISTS users (
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     assets REAL DEFAULT 0.0,
+    first_name VARCHAR(60),
+    last_name VARCHAR(60),
 
     UNIQUE(email)
 );
 
 CREATE TABLE IF NOT EXISTS items (
     id INTEGER PRIMARY KEY,
+    name VARCHAR(60) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    pricture VARCHAR(400) NOT NULL,
     price REAL DEFAULT 0.0,
-    name VARCHAR(255) NOT NULL,
-    pricture VARCHAR(255) NOT NULL,
-    seller_id INTEGER NOT NULL,
-    buyer_id INTEGER DEFAULT NULL,
-    bought DATE DEFAULT NULL,
-    selling_date DATE DEFAULT NULL,
-    description VARCHAR(255) DEFAULT NULL
+
+    UNIQUE(name)
 );
+
+CREATE TABLE IF NOT EXISTS user_stocks (
+    id INTEGER PRIMARY KEY,
+    item_name VARCHAR(60),
+    amount INTEGER DEFAULT 0,
+    buyer_id INTEGER NOT NULL,
+    buy_in_price REAL DEFAULT 0.0,
+    buy_in_date DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS price_log (
+    id INTEGER PRIMARY KEY,
+    item_name VARCHAR(60) NOT NULL,
+    when_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+    old_price REAL DEFAULT 0.0
+);
+
+CREATE TRIGGER IF NOT EXISTS price_update AFTER UPDATE ON items
+BEGIN
+    INSERT INTO price_log(item_name, old_price)
+        VALUES(OLD.name, OLD.price);
+END;
